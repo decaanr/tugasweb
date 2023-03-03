7abef6ff1afe7e1d488c75d3f5ce74c5ce6fcf97
decaanr <selsyaanz15@gmail.com>
2023-03-03 18:03:59 +0700

    v1.2 looping 3

--- tugasjumat.py
+++ tugasjumat.py
@@ -1,6 +1,9 @@
 print('Numbers from 1 to 100')
 n = 1
 while n < 101:
-    if n % 2 == 1:
-        print(n)
+    if n % 2 == 0:
+        print(n, 'adalah bilangan genap')
+    else:
+        print(n, 'adalah bilangan ganjil')
     n = n+1
+
